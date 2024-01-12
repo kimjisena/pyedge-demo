d9b18d9c508113a9b0000d66c76c431a0c7a8609
Kim Jisena <kimjisena@gmail.com>
2024-01-13 00:44:37 +0300

    misc: handle input arguments

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="pyedge", 
+        description="sobel edge detection tool (python and c)"
+    )
+
+    #...
+
+    return parser.parse_args();
+
 def main():
-    pass
+    # parse command line arguments
+    args = parse_arguments();
+
+    #...
 
 if __name__ == '__main__':
     main()
